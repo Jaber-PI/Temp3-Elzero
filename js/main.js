1ef0dcd2bba805dacdcd318fc75a565f21f10169
Jaber-PI <jaberessaber@gmail.com>
2022-11-19 17:23:44 +0100

    created scroll to top icon

--- js/main.js
+++ js/main.js
@@ -7,7 +7,17 @@ window.addEventListener("scroll", () => {
     scroller.style.width = scrollerWidth + "%";
   });
 
-
+// Scroll To Top
+const upButton = document.querySelector(".scrollToTop");
+upButton.addEventListener("click", () => {
+  window.scrollTo({
+    top: 0,
+    behavior: "smooth",
+  });
+})
+window.onscroll = function () {
+  this.scrollY >= 1000 ? upButton.style.right = "20px" : upButton.style.right = "-30px";
+};
 
 
 
